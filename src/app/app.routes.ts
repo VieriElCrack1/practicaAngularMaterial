5f6f212c549fbd3f4a0dc86047e002fbb4a5be22
VieriElCrack1 <santosvieri9@gmail.com>
2024-06-21 16:17:02 -0500

    terminado rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { authGuard } from './auth.guard';
+import { Ejericico1Component } from './dashboard/ejericico1/ejericico1.component';
+import { Ejericico2Component } from './dashboard/ejericico2/ejericico2.component';
+import { NotFundComponent } from './not-fund/not-fund.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "login", component: LoginComponent},
+    {path: "dashboard", component: DashboardComponent, 
+        canActivate: [authGuard],
+        children: [
+            {path: "ejercicio1", component: Ejericico1Component},
+            {path: "ejercicio2", component: Ejericico2Component}
+        ]
+    },
+    {path: "", redirectTo: "login", pathMatch: "full"},
+    {path: "**", component: NotFundComponent}
+];
