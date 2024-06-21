b4a23f5daca1eda9044558921646ebc3560de27d
VieriElCrack1 <santosvieri9@gmail.com>
2024-06-21 16:25:31 -0500

    usando pipe con date con formato dd-MM-yyyy

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -2,17 +2,20 @@ import { Component } from '@angular/core';
 import { ModuleModule } from '../../angular-module/module/module.module';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ModuleModule],
+  imports: [ModuleModule, DatePipe],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
   constructor(private service : AuthService, private router : Router) {}
+
+  fecha = new Date();
 
   logueo(usuario: HTMLInputElement, password: HTMLInputElement) {
     this.service.login(usuario.value, password.value);
